a94d59d1a20d4b878c62eceb1ee650226e472f69
EmreKendirli <emrekenderli@gmail.com>
2023-03-13 03:18:17 +0300

    Delete ve Update işlemleri yapıldı

--- controllers/photoController.js
+++ controllers/photoController.js
@@ -19,7 +19,8 @@ const createPhoto = async (req, res) => {
             name: req.body.name,
             description: req.body.description,
             user: res.locals.user._id,
-            url: result.secure_url
+            url: result.secure_url,
+            image_id:result.public_id
         });
         fs.unlinkSync(req.files.image.tempFilePath)
         res.status(201).redirect("/users/dashboard")
@@ -70,8 +71,64 @@ const getAPhoto = async (req, res) => {
         })
     }
 };
+
+const deletePhoto = async (req, res) => {
+    try {
+        const photo = await Photo.findById(req.params.id);
+        const photoId = photo.image_id;
+
+        await cloudinary.uploader.destroy(photoId);
+        await Photo.findOneAndRemove({_id:req.params.id});
+        res.status(200).redirect("/users/dashboard")
+
+    } catch (error) {
+        res.status(500).json({
+            succeded: false,
+            error
+        })
+    }
+};
+
+const updatePhoto = async (req, res) => {
+    try {
+        const photo = await Photo.findById(req.params.id);
+        
+        if (req.files) {
+            const photoId = photo.image_id;
+            await cloudinary.uploader.destroy(photoId);
+
+            const result = await cloudinary.uploader.upload(
+                req.files.image.tempFilePath, {
+                    use_filename: true,
+                    folder: "lenslight_tr"
+                }
+            )
+
+            photo.url=result.secure_url;
+            photo.image_id=result.public_id
+
+            fs.unlinkSync(req.files.image.tempFilePath)
+        }
+
+        photo.name=req.body.name;
+        photo.description=req.body.description;
+
+        photo.save();
+
+
+        res.status(200).redirect(`/photos/${req.params.id}`)
+
+    } catch (error) {
+        res.status(500).json({
+            succeded: false,
+            error
+        })
+    }
+};
 export {
     createPhoto,
     getAllPhotos,
-    getAPhoto
+    getAPhoto,
+    deletePhoto,
+    updatePhoto
 };
